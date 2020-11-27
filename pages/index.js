9a161342b3873ca3d9facd72f17377849fd7f55e
Philip (flip) Kromer <mrflip@mrflip.com>
2020-11-27 14:32:31 -0600

    Added key packages

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
-// @generated: @expo/next-adapter@2.1.5
-import React                            from 'react'
-import { StyleSheet, Text, View }       from 'react-native'
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Expo + Next.js</Text>
-      <Text>Would you like to buy a car?</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome to Kaiju Wheels</Text>
+        <Text>Would you like to buy a car?</Text>
+      </View>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   text: {
-    fontSize: 16,
-  },
-})
+    fontSize: 16
+  }
+});
