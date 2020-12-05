11b848f489a80b9932c675d986886d037390c8bc
Philip (flip) Kromer <mrflip@mrflip.com>
2020-12-05 17:23:33 -0600

    Separated components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,57 +1,16 @@
 import React /**/ from "react"
 import _ from "lodash"
-import { TextInput as NTextInput } from "react-native"
 //
 import Theme from "../utils/Theme"
 import { useToggler } from "../utils/hooks"
-import {
-  CenterContainer,
-  VContainer,
-  Container,
-  FullWidthOutlineButton,
-  HelperText,
-  KeyboardAwareScrollView,
-  List,
-  Paragraph,
-  StyleSheet,
-  TextInput,
-  Title,
-  View,
-} from "../elements"
+import { FullWidthOutlineButton, KeyboardAwareScrollView, Paragraph, StyleSheet, Title, View } from "../elements"
 //
-import Questions from "../../data/questions.json"
-
-function AnswerBox({ answers }) {
-  return (
-    <View style={{ paddingHorizontal: 16 }}>
-      {answers.map((answer, idx) => (
-        <Paragraph key={answer} style={{ fontSize: 18, marginVertical: 15 }}>
-          &#8226; {answer}
-        </Paragraph>
-      ))}
-    </View>
-  )
-}
+import AnswerBox from "../components/AnswerBox"
+import AnswerEntry from "../components/AnswerEntry"
+import Questions from "../questions.json"
 
 function randomIndex() {
   return _.random(0, Questions.length - 1)
-}
-
-function slugify(str) {
-  return str.replace(/\W+/g, "").toLowerCase()
-}
-
-function answerStyling(guess, answers) {
-  if (answers.some((answer) => slugify(answer) === slugify(guess))) {
-    return styles.happyInput
-  }
-  if (guess.length === 0) {
-    return {}
-  }
-  if (answers.some((answer) => slugify(answer).includes(slugify(guess)))) {
-    return styles.goodStart
-  }
-  return {}
 }
 
 function HomeScreen({ navigation }) {
@@ -61,28 +20,24 @@ function HomeScreen({ navigation }) {
   const textInput = React.useRef(null)
   const { section, subsection, question, answers } = Questions[idx]
   //
-  const successStyle = answerStyling(guess, answers)
-  //
   const nextQuestion = () => {
     setIdx(randomIndex())
     setShown(false)
     setGuess("")
+    textInput.current?.focus()
   }
   //
   return (
     <KeyboardAwareScrollView style={styles.mainScroller}>
       <Title>{question}</Title>
 
-      <View style={[styles.answerBox]}>
-        <HelperText>Answer:</HelperText>
-        <TextInput
-          style={[styles.answerInput, successStyle]}
-          ref={textInput}
-          onSubmitEditing={() => setShown(true)}
-          onChangeText={setGuess}
-          value={guess}
-        />
-      </View>
+      <AnswerEntry
+        ref={textInput}
+        guess={guess}
+        answers={answers}
+        setGuess={setGuess}
+        onSubmitEditing={() => (shown ? nextQuestion() : setShown(true))}
+      />
 
       <FullWidthOutlineButton onPress={toggleShown} title={shown ? "Hide Answers" : "Show Answers"} />
 
@@ -107,23 +62,8 @@ const styles = StyleSheet.create({
     paddingBottom: 80,
     fontStyle: "italic",
   },
-  answerBox: {
-    marginVertical: 20,
-  },
-  answerInput: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-  },
   mainScroller: {
     backgroundColor: "#ccddee",
     padding: 20,
-  },
-  happyInput: {
-    backgroundColor: "#cceecc",
-  },
-  goodStart: {
-    backgroundColor: "#eeeecc",
-    borderColor: "eeee44",
   },
 })
